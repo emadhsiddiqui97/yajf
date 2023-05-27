6d9c42db544951241398fb3ac70bb0573cd83a63
emadhsiddiqui97 <78000913+emadhsiddiqui97@users.noreply.github.com>
2023-05-27 16:15:40 +0500

    changed to ts

--- element.ts
+++ element.ts
@@ -1,5 +1,5 @@
 const div = (strings, ...args) => console.log(strings, args);
-const firstName = 'Emad';
-const lastName  = 'Siddiqui';
+const firstName:string = 'Emad';
+const lastName:string  = 'Siddiqui';
 
 div`Hello ${firstName} ${lastName} !`;
